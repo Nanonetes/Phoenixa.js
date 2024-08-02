d58313099603326c08e66d7026943773aa5834fc
Aditya Dwivedi <dwikaljit@gmail.com>
2024-08-02 18:50:39 +0530

    Update phoenixa.js
    middleware support

--- phoenixa.js
+++ phoenixa.js
@@ -5,6 +5,7 @@ class Phoenixa {
   constructor() {
     this.server = http.createServer();
     this.routes = {};
+    this.middlewares = [];
     
     this.server.on('request', (request, response) => {
       // Set the status code of the response
@@ -13,13 +14,14 @@ class Phoenixa {
         return response;
       };
       
-      // Send JSON data back to the client (for small JSON Data less than highWaterMarkValue)
+      // todo: Send JSON data back to the client (for small JSON Data less
+      //  than highWaterMarkValue)
       response.json = (data) => {
         response.setHeader('Content-Type', 'application/json');
         response.end(JSON.stringify(data));
       };
       
-      // Sending a file back to the client
+      // Sending a file to the client
       response.sendFile = async (path, mime) => {
         const fileHandle = await fs.open(path, 'r');
         const fileStream = fileHandle.createReadStream();
@@ -29,15 +31,33 @@ class Phoenixa {
         fileStream.pipe(response);
       };
       
-      // If the routes object does not have a key of request.method + request.url,
-      // return 404
-      if (!this.routes[request.method.toLowerCase() + request.url]) {
-        return response.status(404).
-            json({error: `Cannot ${request.method} ${request.url}`});
-      }
+      const runMiddleware = (request, response, middlewares, index) => {
+        // Our exit point
+        if (index === middlewares.length) {
+          // If the routes object does not have a key of request.method + request.url,
+          // return 404
+          if (!this.routes[request.method + request.url]) {
+            return response.status(404).
+                json({error: `Cannot ${request.method} ${request.url}`});
+          }
+          
+          // Middlewares have completed their jobs, now we want to run routes.
+          this.routes[request.method + request.url](request,
+              response);
+        } else {
+          
+          // We have more middlewares to run, so continue running them
+          middlewares[index](request, response, () => {
+            runMiddleware(
+                request,
+                response,
+                middlewares,
+                index + 1);
+          });
+        }
+      };
       
-      this.routes[request.method.toLowerCase() + request.url](request,
-          response);
+      runMiddleware(request, response, this.middlewares, 0);
     });
   }
   
@@ -45,6 +65,10 @@ class Phoenixa {
     this.server.listen(port, () => {
       callback();
     });
+  }
+  
+  beforeEach(callback) {
+    this.middlewares.push(callback);
   }
   
   route(method, route, callback) {
